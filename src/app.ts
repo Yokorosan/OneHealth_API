aa24ea295942d2d941f13af2b4d9001eb9a165ae
hermlandim <hermerson_landim@hotmail.com>
2023-01-10 18:37:45 -0300

    create login

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import handleError from "./errors/handleError";
+import { sessionRoutes } from "./routers/session.routers";
 
 const app = express();
 app.use(express.json());
+
+app.use("/login", sessionRoutes);
 
 app.use(handleError);
 
