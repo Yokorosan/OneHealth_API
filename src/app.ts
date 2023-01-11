322608c74254b74da747f75a18d2ace0e7d77d33
Diego Delli Colli <dellicolli@gmail.com>
2023-01-11 15:37:30 -0300

    feat/create address | update address

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import handleError from "./errors/handleError";
+import addressRoutes from "./routers/address.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use(handleError);
+app.use("/address", addressRoutes);
 
 export default app;
