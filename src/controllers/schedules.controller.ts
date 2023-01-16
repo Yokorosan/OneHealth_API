7acff965174ab337e15979de50f3c6915ee449a2
felipebravo <felipe_bravo@hotmail.com>
2023-01-16 14:37:07 -0300

    feat: add delete schedule route

--- src/controllers/schedules.controller.ts
+++ src/controllers/schedules.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { IScheduleRequest } from "../interfaces/schedules/schedules.interface";
 import createSchedulesService from "../services/schedules/createSchedules.service";
+import { deleteScheduleService } from "../services/schedules/deleteSchedule.service";
 
 export const createSchedulesController = async (
   req: Request,
@@ -11,4 +12,10 @@ export const createSchedulesController = async (
   const data = await createSchedulesService(schedule);
 
   return res.status(201).json(data);
+};
+
+export const deleteScheduleController = async (req: Request, res: Response) => {
+  await deleteScheduleService(req.params.id);
+
+  return res.status(204).send();
 };
